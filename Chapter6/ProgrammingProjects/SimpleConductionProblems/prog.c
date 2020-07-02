6e578a9d5703cd94830dcb2a20e4ecbd1967eb40
Mule-skinner-rr <60439533+Mule-skinner-rr@users.noreply.github.com>
2020-07-02 00:55:51 -0700

    slight refactor
    /*
     * Hey Nikhil,
     *
     * Here is a refactor of the SimpleConduction problem
     * from Chapter6. I wrote it with a 'switch' statement
     * to reduce the number of 'if/else' sequences and
     * pointers.  And, I rewrote the calculation functions
     * to work as non-pointer functions because they were
     * only returning one variable.
     *
     * I hope you like the changes and add the program to
     * your collection.  Please feel free to make any
     * changes or suggest them to me.
     *
     * Thank you,
     *
     * Ryan
     */

--- Chapter6/ProgrammingProjects/SimpleConductionProblems/prog.c
+++ Chapter6/ProgrammingProjects/SimpleConductionProblems/prog.c
@@ -3,109 +3,171 @@
 *Regd No - 1641012040
 *Desc -	Simple conduction problems
 */
+
 #include <stdio.h>
-void calc_a(double m, double *area, double del_t, double k, double heat_transfer)
+
+void header(void)
 {
-	*area = (heat_transfer * m) / (k * del_t);
+    printf("\n        kA (T2 - T1)   ");
+    printf("\n   H = --------------  ");
+    printf("\n             X         ");
 }
 
-void calc_k(double m, double area, double del_t, double *k, double heat_transfer)
+double calc_h(double h, double k, double a, double del_t, double x)
 {
-	*k = (heat_transfer * m) / (area * del_t);
+    h = (k * a * del_t) / x;
+    
+    return(h);
 }
 
-void calc_h(double m, double area, double del_t, double k, double *heat_transfer)
+double calc_k(double h, double k, double a, double del_t, double x)
 {
-	*heat_transfer = (k * area * del_t) / m;
+    k = (h * x) / (a * del_t);
+    
+    return(k);
 }
 
-void calc_m(double *m, double area, double del_t, double k, double heat_transfer)
+double calc_a(double h, double k, double a, double del_t, double x)
 {
-	*m = (k * area * del_t) / heat_transfer;
+    a = (h * x) / (k * del_t);
+    
+    return(a);
 }
 
-void calc_t2(double m, double area, double t1, double *t2, double k, double heat_transfer)
+double calc_t2(double h, double k, double a, double t1, double t2, double x)
 {
-	*t2 = ((heat_transfer * m) / (k * area)) + t1;
+    t2 = ((h * x) / (k * a)) + t1;
+    
+    return(t2);
 }
 
-void calc_t1(double m, double area, double *t1, double t2, double k, double heat_transfer)
+double calc_t1(double h, double k, double a, double t1, double t2, double x)
 {
-	*t1 = t2 - ((heat_transfer * m) / (k * area));
+    t1 = t2 - ((h * x) / (k * a));
+    
+    return(t1);
 }
+
+double calc_x(double h, double k, double a, double del_t, double x)
+{
+    x = (k * a * del_t) / h;
+    
+    return(x);
+}
+
+void display_solution(double h, double k, double a, double t2, double t1, double x)
+{
+    printf("\n  H = %.1f W.", h);
+    printf("\n  k = %.3f W/m-K.", k);
+    printf("\n  A = %.3f m^2.", a);
+    printf("\n  T2 = %.0f K.", t2);
+    printf("\n  T1 = %.0f K.", t1);
+    printf("\n  x = %.3f m.\n\n", x);
+}
+
+char get_info(double*, double*, double*, double*, double*, double*);
+double find_unk(char, double, double, double, double, double, double);
 
 void main()
 {
-	double heat_transfer, coefficient, cross_area, T1, T2, thickness;
-	int c1, c2, c3, c4, c5, c6;
-	char ch;
-	printf("\nRespond to the prompts with the data known. For the unknown quantity, enter a question mark (?).");
-	
-	printf("\nRate of heat transfer (watts) >> ");
-	c1 = scanf(" %lf", &heat_transfer);
-	if(c1 == 0)
-		scanf("%c", &ch);
-		
-	printf("\nCoefficient of thermal conductivity (W/m-K) >> ");
-	c2 = scanf(" %lf", &coefficient);
-	if(c2 == 0)
-		scanf("%c", &ch);
-		
-	printf("\nCross-sectional area of conductor (m^2) >> ");
-	c3 = scanf(" %lf", &cross_area);
-	if(c3 == 0)
-		scanf("%c", &ch);
-	
-	printf("\nTemperature on one side (K) >> ");
-	c4 = scanf(" %lf", &T2);
-	if(c4 == 0)
-		scanf("%c", &ch);
-	
-	printf("\nTemperature on other side (K) >> ");
-	c5 = scanf(" %lf", &T1);
-	if(c5 == 0)
-		scanf("%c", &ch);
-	
-	printf("\nThickness of conductor (m) >> ");
-	c6 = scanf(" %lf", &thickness);
-	if(c6 == 0)
-		scanf("%c", &ch);
-	
-	if(c1 == 0)
-		calc_h(thickness, cross_area, (T2 - T1), coefficient, &heat_transfer);
-	else if(c2 == 0)
-		calc_k(thickness, cross_area, (T2 - T1), &coefficient, heat_transfer);
-	else if(c3 == 0)
-		calc_a(thickness, &cross_area, (T2 - T1), coefficient, heat_transfer);
-	else if(c4 == 0)
-		calc_t2(thickness, cross_area, T1, &T2, coefficient, heat_transfer);
-	else if(c5 == 0)
-		calc_t1(thickness, cross_area, &T1, T2, coefficient, heat_transfer);
-	else if(c6 == 0)
-		calc_m(&thickness, cross_area, (T2 - T1), coefficient, heat_transfer);
-	
-	printf("\n\n%5ckA (T2 - T1)", ' ');
-	printf("\nH = ----------------");
-	printf("\n%10cX", ' ');
-	
-	if(c1 == 0)
-		printf("\nRate of heat transfer is %.3f W.", heat_transfer);
-	else if(c2 == 0)
-		printf("\nCoefficient of thermal conductivity is %.3f W/m-K.", coefficient);
-	else if(c3 == 0)
-		printf("\nCross-sectional area of conductor is %.3f m^2.", cross_area);
-	else if(c4 == 0)
-		printf("\nTemperature on the other side is %.0f K.", T2);
-	else if(c5 == 0)
-		printf("\nTemperature on the other side is %.0f K.", T1);
-	else if(c6 == 0)
-		printf("\nThickness of conductor is %.4f m.", thickness);
-		
+    double unk, h, k, a, t2, t1, x;
+    char calc;
+    
+    calc = get_info(&h, &k, &a, &t2, &t1, &x);
+    
+    header();
+    
+    unk = find_unk(calc, h, k, a, t2, t1, x);
+    
+}
 
-		printf("\nH = %.3f W.", heat_transfer);
-		printf("\nk = %.3f W/m-K.", coefficient);
-		printf("\nA = %.3f m^2.", cross_area);
-		printf("\nT2 = %.0f K.", T2);
-		printf("\nT1 = %.0f K.", T1);
-		printf("\nm = %.4f m.", thickness);
+char get_info(double *h, double *k, double *a, double *t2, double *t1, double *x)
+{
+    int flag;
+    char ch, calc = 0;
+    
+    printf("\nRespond to the prompts with the data known. For the unknown quantity, enter a question mark (?).");
+    
+    printf("\nRate of heat transfer (watts) >> ");
+    flag = scanf("%lf", h);
+    if (flag == 0) {
+        scanf(" %c", &ch);
+        calc = 'h';
+    }
+    printf("\nCoefficient of thermal conductivity (W/m-K) >> ");
+    flag = scanf("%lf", k);
+    if (flag == 0){
+        scanf(" %c", &ch);
+        calc = 'k';
+    }
+    printf("\nCross-sectional area of conductor (m^2) >> ");
+    flag = scanf("%lf", a);
+    if (flag == 0){
+        scanf(" %c", &ch);
+        calc = 'a';
+    }
+    printf("\nTemperature on one side (K) >> ");
+    flag = scanf("%lf", t2);
+    if (flag == 0){
+        scanf(" %c", &ch);
+        calc = '2';
+    }
+    printf("\nTemperature on other side (K) >> ");
+    flag = scanf("%lf", t1);
+    if (flag == 0){
+        scanf(" %c", &ch);
+        calc = '1';
+    }
+    printf("\nThickness of conductor (m) >> ");
+    flag = scanf("%lf", x);
+    if (flag == 0){
+        scanf(" %c", &ch);
+        calc = 'x';
+    }
+    return(calc);
+}
+
+double find_unk(char calc, double h, double k, double a, double t2, double t1, double x)
+{
+    double unk = 0.0;
+    
+    switch (calc)
+    {
+        case 'h':
+            unk = calc_h(h, k, a, (t2 - t1), x);
+            printf("\n\nThe rate of heat transfer is %.1f W\n", unk);
+            display_solution(unk, k, a, t2, t1, x);
+            break;
+            
+        case 'k':
+            unk = calc_k(h, k, a, (t2 - t1), x);
+            printf("\n\nThe coefficient of thermal conductivity is %.3f\n", unk);
+            display_solution(h, unk, a, t2, t1, x);
+            break;
+            
+        case 'a':
+            unk = calc_a(h, k, a, (t2 - t1), x);
+            printf("\n\nThe cross-sectional area of the conductor is %.3f\n", unk);
+            display_solution(h, k, unk, t2, t1, x);
+            break;
+            
+        case '2':
+            unk = calc_t2(h, k, a, t1, t2, x);
+            printf("\n\nThe temperature on one side is %.0f\n", unk);
+            display_solution(h, k, a, unk, t1, x);
+            break;
+            
+        case '1':
+            unk = calc_t1(h, k, a, t1, t2, x);
+            printf("\n\nThe temperature on the other side is %.0f\n", unk);
+            display_solution(h, k, a, t2, unk, x);
+            break;
+            
+        case 'x':
+            unk = calc_x(h, k, a, (t2 - t1), x);
+            printf("\n\nThe thickness of the conductor is %.3f\n", unk);
+            display_solution(h, k, a, t2, t1, unk);
+            break;
+    }
+    return(unk);
 }
