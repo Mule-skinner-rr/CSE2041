77e6fc52251af4c5ad1830aaf2bf345c08a4b25c
Ryan Roderick <60439533+ryanroderick1@users.noreply.github.com>
2020-09-25 04:33:30 -0700

    Add input functionality to prog.c
    Hey Nikhil,
    
    Your program helped me get started solving this problem, because I was having trouble with that. 
    
    While looking at you program I noticed that part of the problem states that "Function fgetAnswers can also be called to input a student's answers," p. 443 in Hanly and Koffman, 7th Ed.
    
    I added this functionality using the 'fopen' append option, with the "a" in place of the "r" or "w", and I thought it might be fun to see if you would add it in.
    
    Please do!
    
    Thank you,
    
    Ryan

--- Chapter7/ProgrammingProjects/MultipleChoiceExam/prog.c
+++ Chapter7/ProgrammingProjects/MultipleChoiceExam/prog.c
@@ -32,6 +32,65 @@ void fgetAnswers(char answers[], int n, FILE *input)
 	
 	int missed[n], i, id, correct;
 	char choice;
+	char space, y_or_n;
+	
+	/* BEGIN: Added functionality... */
+	char space, y_or_n;
+
+    printf("Do you have any more scores to enter?\n");
+    printf("Please enter 'y' or 'n'> ");
+    scanf(" %c", &y_or_n);
+
+    if(y_or_n == 'y') {
+        fclose(input);
+        /* closing 'input' pointer so I can re-open it as an append file */
+        
+        input = fopen("examdat_1G.txt", "a");
+        /* This opens "examdat_1G" for more data.  "a" stands for append. */
+        
+        if(input == NULL)
+            perror("input didn't open\n");
+        /* checking if 'input' file opened. */
+        
+        fprintf(input, "\n");
+        /* appends a line escape sequence to the end of "examdat_1G.txt", which assumes the insertion point is at the end of the last line instead of at the beginning of a new one. */
+        do {
+            printf("\nPlease enter the ID# > ");
+            scanf("%d", &id);
+            fprintf(input, "%d ", id);
+            /* Scans in the id# with a space and appends it to the 'input' file. */
+            
+            for(i=0; i<n; ++i) {
+                printf("\nPlease enter each letter answer followed by the \"return\" key\n> ");
+                scanf(" %c", &choice);
+                fprintf(input, "%c", choice);
+            }
+            fprintf(input,"\n");
+            
+            printf("Do you have any more scores to enter?\n");
+            printf("Please enter 'y' or 'n'> ");
+            scanf(" %c", &y_or_n);
+            
+        } while(y_or_n == 'y');
+        
+        fclose(input);
+        /* closes 'input' so the program can re-open it as a read, "r", file. */
+        
+        input = fopen("examdat_1G.txt", "r");
+        
+        if(input == NULL)
+            perror("input didn't open\n");
+        
+        fscanf(input, "%d", &n);
+        fscanf(input, "%c", &space);
+        /* re-opens input file with appended information */
+        
+        for (i=0; i<n; ++i) {
+            fscanf(input, "%c", &answers[i]);
+        }
+        /* Re-scans the first number and five letters in the file so that the program does not print them as a student ID and quiz responses. Closing and reopening "examdat.txt" as a read file causes the compiler to read from the beginning of the file instead of where it left off in in the main funciton. */
+    }
+	/* ...END: Added functionality */
 	
 	for(i = 0; i < n; i++)
 	{
